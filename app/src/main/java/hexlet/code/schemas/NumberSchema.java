626b5b212aecacfc6d4bff5cc56f1b79fa194e7c
Obydennov Dmitrii <dima-obydennov@mail.ru>
2023-11-26 22:12:46 +0300

    add new

--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -1,12 +1,22 @@
 package hexlet.code.schemas;
 
 public class NumberSchema extends BaseSchema {
+    /**
+     * Устанавливает условие, что число должно быть положительным.
+     * @return Текущий объект NumberSchema для цепочечных вызовов.
+     */
     public NumberSchema positive() {
         this.predicate = value ->
                 value == null || (value instanceof Number && ((Number) value).intValue() > 0);
         return this;
     }
 
+    /**
+     * Устанавливает диапазон, в пределах которого должно находиться число.
+     * @param min Минимальное значение.
+     * @param max Максимальное значение.
+     * @return Текущий объект NumberSchema для цепочечных вызовов.
+     */
     public NumberSchema range(int min, int max) {
         this.predicate = this.predicate.and(value ->
                 value == null || (value instanceof Number
